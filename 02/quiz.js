d635f417276c4aa4dd4ac8bdb57d58f329ae16c1
wkdlfhtm1 <31677241+wkdlfhtm1@users.noreply.github.com>
2018-09-07 18:26:06 +0900

    2016108259 KimSeungHyun

---
+++ 02/quiz.js
@@ -0,0 +1,28 @@
+for(var i = 1; i <=100; i++)
+{
+    console.log(i);
+}
+
+
+
+for(var i = 1; i <= 100; i++)
+{
+    if(i % 2 == 0)
+    {
+        console.log("짝수"+ i)
+        
+    }
+    if(i % 2 !== 0)
+    {
+        console.log("홀수"+ i)
+    }
+}
+
+for(var i = 1; i <= 9; i++)
+{
+    for(j = 1; j <= 9; j++)
+    {
+        
+       alert(i + '*' + j + '=' + i*j)
+    }
+}
