8af7b5eb273aef1b7aaee8c0f42b90f97088bb02
wkdlfhtm1 <31677241+wkdlfhtm1@users.noreply.github.com>
2018-10-06 04:08:28 +0900

    6차

--- 06/app.js
+++ 06/app.js
@@ -1,21 +1,67 @@
-console.log('app');
 
-var appkey = 'da4aeaab9187f8ef34137a29ca6b7468';
-var query = '제주대학교';
-var url = `https://dapi.kakao.com/v2/search/web?query=${query}`;
+  
+  var submitbutton= document.querySelector('#submit-window');
+  var addpage = document.getElementById('addpage');
+  
+  var page = 1;
+  var url = `https://dapi.kakao.com/v2/search/web`
+  var myHeaders = new Headers();
 
-var myHeaders = new Headers();
-myHeaders.append('Authorization','KakaoAK da4aeaab9187f8ef34137a29ca6b7468');
-var options = {
+
+  myHeaders.append('Authorization','KakaoAK da4aeaab9187f8ef34137a29ca6b7468');
+
+
+  var options = {
     headers: myHeaders
-};
+  };
 
 
-
-
-fetch(url, options)
+function getdata(){
+  var query = document.querySelector('#search-window').value;
+  fetch(url+`?query=${query}&page=${page}`, options)
   .then(function(response){
     response.json().then(function(data){
-      console.log('json data:',data);
+    searched(data);
     });
+  }) 
+}
+
+
+submitbutton.addEventListener('click',function(){
+  document.getElementById('searched').innerHTML = '';
+  getdata();
   })
+
+
+
+
+function searched(data){
+  
+  var str = '';
+  for(var i = 0; i < data.documents.length; i++)
+  {
+    var title = data.documents[i].title;
+    var contents = data.documents[i].contents;
+    var datetime = new Date(data.documents[i].datetime);
+    var urls = data.documents[i].url;
+  
+    str += `
+            <a id='title' href=${urls} target="_blank">
+            <div id = 'searchedlist'>
+            <div id = 'titles'>${title}</div>
+            <div id = 'contents'>${contents}</div>
+            <div id = 'datetime'>${datetime.getFullYear()}년 ${datetime.getMonth()}월 ${datetime.getDay()}일</div>
+            </div>
+            </a>
+            `
+    
+  }
+  document.getElementById('searched').innerHTML += str;
+  
+}
+
+addpage.addEventListener('click',function(){
+  page++;
+  getdata();
+})
+
